4ba2eb09ba7c604cfd8f6869670f9830fa07cb75
221840039 <1312876092@qq.com>
2024-01-26 21:54:28 +0800

    modify

--- gpt.py
+++ gpt.py
@@ -1,6 +1,6 @@
 from con1 import Con
 
-# mode_available_now："gpt-3.5-turbo"，
 if __name__ == '__main__':
     newCon = Con("dall-e-2", "You are a helpful assistant")
     print(newCon.textToImage("a little black cat",1))
+    
