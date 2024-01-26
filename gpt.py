a29fde6a4c5433930435f077091318241767ff29
221840039 <1312876092@qq.com>
2024-01-26 21:04:10 +0800

    gpt3.5 TextToText

--- gpt.py
+++ gpt.py
@@ -3,3 +3,4 @@ if __name__ == '__main__':
     newCon = Con("gpt-3.5-turbo", "You are a helpful assistant")
     print(newCon.textToText("describe yourself please"))
     print(newCon.textToText("recite 10 digits of PI for me, please"))
+    print(newCon.textToText("Did you remember the last question i ask you?"))
