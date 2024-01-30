e082cc0fdad143c54262622256ed578669c51005
frosty <3313773846@qq.com>
2024-01-30 16:41:02 +0800

    1

--- text_process.py
+++ text_process.py
@@ -1,5 +1,5 @@
 import os
-from AiApi import con1
+import con1
 
 key = "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"
 # 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
@@ -7,9 +7,6 @@ gpt = con1.Con("gpt-3.5-turbo", "a text analysis assistant")
 
 path = "novels/变色龙.txt"
 prompt = "对以下小说文本进行分析，以中文的json格式回答我，包含主人公、场景、配角、小说情感基调、重要情节段落：\n"
-
-os.environ["HTTP\_PROXY"] = "127.0.0.1"
-os.environ["HTTPS\_PROXY"] = "127.0.0.1"
 
 
 def analyse():
