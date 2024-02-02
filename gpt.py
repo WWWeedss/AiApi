4c672487a9d7b89b210fbbb72e4635ecd7f9ca2e
221840039 <1312876092@qq.com>
2024-02-02 17:44:19 +0800

    mask

--- gpt.py
+++ gpt.py
@@ -1,5 +1,8 @@
 from con1 import Con
 
 if __name__ == '__main__':
-    newCon = Con("dall-e-2", "You are a helpful assistant")
-    print(newCon.textToImage("a little black cat",1))
+    newCon = Con("gpt-3.5-turbo", "You are a helpful assistant")
+    newCon.create_assistant("readRobot", "You are a knowledgeable reading analyst")
+    file_ids = [newCon.open_file("assistants", "Test.txt")]
+    newCon.add_assistant_file("请告诉我这个txt文件里描述了哪些种族", file_ids)
+    print(newCon.run_assistant("请称呼user为顾蕾"))
